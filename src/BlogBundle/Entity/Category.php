09cd66b528b322db6a570ab50a08b42162a24385
khvorostianenko <mikhail.khvorostianenko@gmail.com>
2016-11-14 00:52:01 +0200

    relation beetween Category and News added

---
+++ src/BlogBundle/Entity/Category.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+/**
+ * Category
+ */
+class Category
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $news;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->news = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add news
+     *
+     * @param \BlogBundle\Entity\News $news
+     *
+     * @return Category
+     */
+    public function addNews(\BlogBundle\Entity\News $news)
+    {
+        $this->news[] = $news;
+
+        return $this;
+    }
+
+    /**
+     * Remove news
+     *
+     * @param \BlogBundle\Entity\News $news
+     */
+    public function removeNews(\BlogBundle\Entity\News $news)
+    {
+        $this->news->removeElement($news);
+    }
+
+    /**
+     * Get news
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getNews()
+    {
+        return $this->news;
+    }
+}
